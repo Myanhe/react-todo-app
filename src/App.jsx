8c32e828ba2f92409b386ff4e6dc38ea4fd35816
Myanhe <coldzan06@gmail.com>
2024-06-07 09:37:45 +0700

    merubah direktori dari react-todo-app/react-todo-app menjadi react-todo-app

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Guru Domba',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false,
+    },
+  ])
+
+  console.log(todos)
+
+ 
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      {/* Gunakan method map di sini */}
+      {todos.map((todo) => {
+        return <p key={todo.id}>{todo.title}</p>
+      })}
+    </div>
+  )
+}
+
+export default App
